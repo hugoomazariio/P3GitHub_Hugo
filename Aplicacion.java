f638748c3a6181477835364abaccfe3fe0444399
CarlosMatellano <carlos.matellano.v@gmail.com>
2024-03-08 11:22:41 +0100

    Cambios Aplicacion.java
    Cambiamos texto

--- Aplicacion.java
+++ Aplicacion.java
@@ -10,12 +10,12 @@ public class Ejercicio1 {
         public static void main(String[] args) {
             Scanner entrada= new Scanner (System.in);
             
-            System.out.println("Introduce un número");  
+            System.out.println("Escribe un número");  
             int numero1 = entrada.nextInt ();
-            System.out.println("Introuzca otro número");
+            System.out.println("Escribe otro número");
             int numero2=entrada.nextInt ();
             
             int suma=numero1+numero2;
-            System.out.println("El resultado de la suma es "+suma);
+            System.out.println("Resultado= "+suma);
         }
 }
