4355e0b7becad0fd0eff8fb734cf02c1cc7a8e56
CarlosMatellano <carlos.matellano.v@gmail.com>
2024-03-08 11:28:18 +0100

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -10,12 +10,12 @@ public class Ejercicio1 {
         public static void main(String[] args) {
             Scanner entrada= new Scanner (System.in);
             
-            System.out.println("Escribe un número");  
+            System.out.println("Escribe el 1º número");  
             int numero1 = entrada.nextInt ();
-            System.out.println("Escribe otro número");
+            System.out.println("Escribe el 2ºnúmero");
             int numero2=entrada.nextInt ();
             
             int suma=numero1+numero2;
-            System.out.println("Resultado= "+suma);
+            System.out.println("Resultado de la suma= "+suma);
         }
 }
